1a9c0da9496b9dab4b615b3f324e59958ca54b71
PavkataXD74 <101363188+PavkataXD74@users.noreply.github.com>
2022-03-11 15:05:36 +0200

    Version 0.1
    so that everyone has the same code

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.get("/", async (_, res) => {
 
 	res.render("pages/index", {
 		title: "SpaceTime",
-		stylesheet: "css/index.css",
+		stylesheet: "css/style.css",
 		rocketData: JSON.stringify(rocketData),
 	});
 });
