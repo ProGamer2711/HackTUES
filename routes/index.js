2a98c491f33a8bdce16dd6a47455dde189ee1cd6
ProGamer2711 <alexandurshestakov@gmail.com>
2022-03-13 07:25:58 +0200

    last moment changes

--- routes/index.js
+++ routes/index.js
@@ -14,10 +14,26 @@ router.get("/", async (_, res) => {
 	});
 });
 
-router.post("/next", async (req, res) => {
-	const next = req.body["next-url"];
+router.post("/", async (req, res) => {
+	const query = req.body.query;
 
-	const rocketData = await getLaunchData(next);
+	const rocketData = await getLaunchData(
+		`https://lldev.thespacedevs.com/2.2.0/launch/upcoming?mode=list&limit=3&search=${query}`
+	);
+
+	if (!rocketData) return res.redirect("/no_results_found");
+
+	res.render("pages/index", {
+		title: "SpaceTime",
+		data: JSON.stringify(rocketData),
+		query,
+	});
+});
+
+router.post("/previous", async (req, res) => {
+	const previous = req.body["previous-url"];
+
+	const rocketData = await getLaunchData(previous);
 
 	if (!rocketData) return res.redirect("/no_results_found");
 
@@ -28,10 +44,10 @@ router.post("/next", async (req, res) => {
 	});
 });
 
-router.post("/previous", async (req, res) => {
-	const previous = req.body["previous-url"];
+router.post("/next", async (req, res) => {
+	const next = req.body["next-url"];
 
-	const rocketData = await getLaunchData(previous);
+	const rocketData = await getLaunchData(next);
 
 	if (!rocketData) return res.redirect("/no_results_found");
 
